b2a0abbdc781c4f26604c312d7e0d4f409bd60f7
jakub <jakub.harchut@gmail.com>
2026-03-04 13:57:32 +0100

    Przeniesienie plików PWA do gównego folderu

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'snooker-lab-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+// Instalacja Service Workera i buforowanie plików
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Aktywacja i czyszczenie starych wersji
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Obsługa zapytań sieciowych (Network-First dla aktualizacji)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
